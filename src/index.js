6e47bea6ef2faa3655260a594b31a29e444b6a5f
lhasselman <124750889+lhasselman@users.noreply.github.com>
2023-03-30 21:18:31 -0500

    forecast HTML to JS

--- src/index.js
+++ src/index.js
@@ -42,6 +42,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 //
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+//
 let apiKey = "97f8e93f00107773f88eafd933ce86b7";
 let apiEndpoint = "https://api.openweathermap.org/data/2.5/";
 let unit = `metric`;
@@ -144,6 +174,8 @@ function displayTemperatureCelsius(event) {
   fahrenheitLink.classList.remove("active");
   tempElement.innerHTML = Math.round(celsiusTemperature);
 }
+//
+displayForecast();
 //
 let celsiusTemperature = null;
 //
